57ed7fd7afed6ad13603d02f032d9f645b0e5c6e
Stasysolnze <Stasysolnze@yandex.ru>
2021-12-02 11:51:53 +0300

    Добавьте файлы проекта.

---
+++ Homework_4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            int x = 0;
+            Console.Write("Введите N: ");
+
+            N = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= N; i++)
+            {
+                x = x + (2 * i - 1);
+                Console.Write(x);
+                Console.Write(" ");
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
